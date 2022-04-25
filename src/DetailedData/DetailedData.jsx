0b6cf643bca52740ea1ce81a8dfd8c278acc1bde
frinica <frida.nicander@chasacademy.se>
2022-04-25 17:17:51 +0200

    add icons to DetailedData

--- src/DetailedData/DetailedData.jsx
+++ src/DetailedData/DetailedData.jsx
@@ -5,21 +5,44 @@ import "../bootstrap-icons.css";
 const DetailedData = ({ weatherData }) => {
   return (
     <div className="detailedData">
-      <div className="temperature">
-        <p>{weatherData[0].main.temp_max}</p>
-        <p>{weatherData[0].main.temp_min}</p>
+      <div className="dataHeading">
+        <h2>Details</h2>
       </div>
-      <div className="weather">
-        <p>{weatherData[0].main.humidity}%</p>
-        <p>{weatherData[0].wind.speed} m/s</p>
-      </div>
-      <div className="sunState">
-        <p>
-          Sunrise: {new Date(weatherData[0].sys.sunrise).toLocaleTimeString()}
-        </p>
-        <p>
-          Sunset: {new Date(weatherData[0].sys.sunset).toLocaleTimeString()}
-        </p>
+      <div className="details">
+        <div className="temperature">
+          <div className="max-temp">
+            <i class="bi bi-thermometer-high detail-icon" />
+            <span>{weatherData[0].main.temp_max}</span>
+          </div>
+          <div className="min-temp">
+            <i class="bi bi-thermometer detail-icon" />
+            <span>{weatherData[0].main.temp_min}</span>
+          </div>
+        </div>
+        <div className="weather">
+          <div className="humidity">
+            <i class="bi bi-water detail-icon" />
+            <span>{weatherData[0].main.humidity}%</span>
+          </div>
+          <div className="wind">
+            <i class="bi bi-wind detail-icon" />
+            <span>{weatherData[0].wind.speed} m/s</span>
+          </div>
+        </div>
+        <div className="sunState">
+          <div className="sunrise">
+            <i class="bi bi-sunrise detail-icon" />
+            <span>
+              {new Date(weatherData[0].sys.sunrise).toLocaleTimeString()}
+            </span>
+          </div>
+          <div className="sunset">
+            <i class="bi bi-sunset detail-icon" />
+            <span>
+              {new Date(weatherData[0].sys.sunset).toLocaleTimeString()}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
